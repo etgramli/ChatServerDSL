e18935bd656bce190ff887cd19177111b496465a
etgramli <etgramlich@gmx.de>
2018-04-09 11:52:24 +0200

    Implemented VoicePermissionBuilder

--- src/de/htwg/modellgetrieben/dsl/permission/VoicePermission.kt
+++ src/de/htwg/modellgetrieben/dsl/permission/VoicePermission.kt
@@ -1,16 +1,15 @@
 package de.htwg.modellgetrieben.dsl.permission
 
 data class VoicePermission(
-        override val createInstantInvite: PermissionState,
-        override val manageChannel: PermissionState,
-        override val managePermissions: PermissionState,
-        override val manageWebhooks: PermissionState,
-        val viewChannel: PermissionState,
-        val connect: PermissionState,
-        val speak: PermissionState,
-        val muteMembers: PermissionState,
-        val deafenMembers: PermissionState,
-        val moveMembers: PermissionState,
-        val useVoiceActivity: PermissionState
-
+        override val createInstantInvite: PermissionState = PermissionState.Default,
+        override val manageChannel: PermissionState = PermissionState.Default,
+        override val managePermissions: PermissionState = PermissionState.Default,
+        override val manageWebhooks: PermissionState = PermissionState.Default,
+        val viewChannel: PermissionState = PermissionState.Default,
+        val connect: PermissionState = PermissionState.Default,
+        val speak: PermissionState = PermissionState.Default,
+        val muteMembers: PermissionState = PermissionState.Default,
+        val deafenMembers: PermissionState = PermissionState.Default,
+        val moveMembers: PermissionState = PermissionState.Default,
+        val useVoiceActivity: PermissionState = PermissionState.Default
 ) : Permission
---
+++ src/de/htwg/modellgetrieben/dsl/permission/builders/VoicePermissionBuilder.kt
@@ -0,0 +1,109 @@
+package de.htwg.modellgetrieben.dsl.permission.builders
+
+import de.htwg.modellgetrieben.dsl.permission.PermissionState
+import de.htwg.modellgetrieben.dsl.permission.VoicePermission
+
+
+fun voicepermission(block: VoicePermissionBuilder.() -> Unit): VoicePermission {
+    return VoicePermissionBuilder().apply(block).build()
+}
+
+class VoicePermissionBuilder {
+    private var createInstantInvite: PermissionState = PermissionState.Default
+    private var manageChannel: PermissionState = PermissionState.Default
+    private var managePermissions: PermissionState = PermissionState.Default
+    private var manageWebhooks: PermissionState = PermissionState.Default
+    private var viewChannel: PermissionState = PermissionState.Default
+    private var connect: PermissionState = PermissionState.Default
+    private var speak: PermissionState = PermissionState.Default
+    private var muteMembers: PermissionState = PermissionState.Default
+    private var deafenMembers: PermissionState = PermissionState.Default
+    private var moveMembers: PermissionState = PermissionState.Default
+    private var useVoiceActivity: PermissionState = PermissionState.Default
+
+
+    fun allowCreateInstantInvite() {
+        createInstantInvite = PermissionState.ALLOWED
+    }
+    fun allowManageChannel() {
+        manageChannel = PermissionState.ALLOWED
+    }
+    fun allowManagePermissions() {
+        managePermissions = PermissionState.ALLOWED
+    }
+    fun allowManageWebhooks() {
+        manageWebhooks = PermissionState.ALLOWED
+    }
+    fun allowViewChannel() {
+        viewChannel = PermissionState.ALLOWED
+    }
+    fun allowConnect() {
+        connect = PermissionState.ALLOWED
+    }
+    fun allowSpeak() {
+        speak = PermissionState.ALLOWED
+    }
+    fun allowMuteMembers() {
+        muteMembers = PermissionState.ALLOWED
+    }
+    fun allowDeafenMembers() {
+        deafenMembers = PermissionState.ALLOWED
+    }
+    fun allowMoveMembers() {
+        moveMembers = PermissionState.ALLOWED
+    }
+    fun allowUseVoiceActivity() {
+        useVoiceActivity = PermissionState.ALLOWED
+    }
+
+    fun forbidCreateInstantInvite() {
+        createInstantInvite = PermissionState.FORBIDDEN
+    }
+    fun forbidManageChannel() {
+        manageChannel = PermissionState.FORBIDDEN
+    }
+    fun forbidManagePermissions() {
+        managePermissions = PermissionState.FORBIDDEN
+    }
+    fun forbidManageWebhooks() {
+        manageWebhooks = PermissionState.FORBIDDEN
+    }
+    fun forbidViewChannel() {
+        viewChannel = PermissionState.FORBIDDEN
+    }
+    fun forbidConnect() {
+        connect = PermissionState.FORBIDDEN
+    }
+    fun forbidSpeak() {
+        speak = PermissionState.FORBIDDEN
+    }
+    fun forbidMuteMembers() {
+        muteMembers = PermissionState.FORBIDDEN
+    }
+    fun forbidDeafenMembers() {
+        deafenMembers = PermissionState.FORBIDDEN
+    }
+    fun forbidMoveMembers() {
+        moveMembers = PermissionState.FORBIDDEN
+    }
+    fun forbidUseVoiceActivity() {
+        useVoiceActivity = PermissionState.FORBIDDEN
+    }
+
+
+    fun build(): VoicePermission =
+            VoicePermission(
+                    createInstantInvite,
+                    manageChannel,
+                    managePermissions,
+                    manageWebhooks,
+                    viewChannel,
+                    connect,
+                    speak,
+                    muteMembers,
+                    deafenMembers,
+                    moveMembers,
+                    useVoiceActivity
+            )
+
+}
