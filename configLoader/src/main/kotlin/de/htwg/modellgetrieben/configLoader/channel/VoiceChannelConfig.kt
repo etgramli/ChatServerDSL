3a7b8885b9a1af3228392bc4bc121a828e547231
etgramli <etgramlich@gmx.de>
2018-04-23 20:59:40 +0200

    Added permissions to VoiceChannelConfigImpl

--- configLoader/src/main/kotlin/de/htwg/modellgetrieben/configLoader/channel/VoiceChannelConfig.kt
+++ configLoader/src/main/kotlin/de/htwg/modellgetrieben/configLoader/channel/VoiceChannelConfig.kt
@@ -1,23 +1,23 @@
 package de.htwg.modellgetrieben.configLoader.channel
 
-interface VoiceChannelConfig {
-    fun createVoiceChannel(name: String,
-                           maxUser: Int,
-                           bitrate: Int) {
-        setName(name)
-        setMaxUsers(maxUser)
-        setBitrate(bitrate)
-        //println(voiceChannel.permissions)
-    }
+import de.htwg.modellgetrieben.configLoader.permission.PermissionState
 
-    fun setName (name: String) {
-        println("Voice channel name: $name")
-    }
-    fun setMaxUsers(number: Int) {
-        println("Max number of users: $number")
-    }
-    fun setBitrate(bitrate: Int) {
-        println("Bitrate: $bitrate")
-    }
-    // ToDo: permissions: VoicePermission,
+interface VoiceChannelConfig {
+    fun createVoiceChannel(name: String, maxUser: Int, bitrate: Int)
+
+    fun setName (name: String)
+    fun setMaxUsers(number: Int)
+    fun setBitrate(bitrate: Int)
+
+    fun setCreateInstantInvite(state: PermissionState)
+    fun setManageChannel(state: PermissionState)
+    fun setManagePermissions(state: PermissionState)
+    fun setManageWebhooks(state: PermissionState)
+    fun setViewChannel(state: PermissionState)
+    fun setConnect(state: PermissionState)
+    fun setSpeak(state: PermissionState)
+    fun setMuteMembers(state: PermissionState)
+    fun setDeafenMembers(state: PermissionState)
+    fun setMoveMembers(state: PermissionState)
+    fun setUseVoiceActivity(state: PermissionState)
 }
--- configLoader/src/main/kotlin/de/htwg/modellgetrieben/configLoader/mockImpl/VoiceChannelConfigImpl.kt
+++ configLoader/src/main/kotlin/de/htwg/modellgetrieben/configLoader/mockImpl/VoiceChannelConfigImpl.kt
@@ -1,21 +1,58 @@
 package de.htwg.modellgetrieben.configLoader.mockImpl
 
 import de.htwg.modellgetrieben.configLoader.channel.VoiceChannelConfig
+import de.htwg.modellgetrieben.configLoader.permission.PermissionState
 
 class VoiceChannelConfigImpl(name: String) : VoiceChannelConfig {
     override fun createVoiceChannel(name: String, maxUser: Int, bitrate: Int) {
-        super.createVoiceChannel(name, maxUser, bitrate)
+        setName(name)
+        setMaxUsers(maxUser)
+        setBitrate(bitrate)
     }
 
     override fun setName(name: String) {
-        super.setName(name)
+        println("Voice channel name: $name")
     }
 
     override fun setMaxUsers(number: Int) {
-        super.setMaxUsers(number)
+        println("Max users: $number")
     }
 
     override fun setBitrate(bitrate: Int) {
-        super.setBitrate(bitrate)
+        println("Bitrate: $bitrate")
+    }
+
+    override fun setCreateInstantInvite(state: PermissionState) {
+        println("createInstantInvite: $state")
+    }
+    override fun setManageChannel(state: PermissionState) {
+        println("manageChannel: $state")
+    }
+    override fun setManagePermissions(state: PermissionState) {
+        println("managePermissions: $state")
+    }
+    override fun setManageWebhooks(state: PermissionState) {
+        println("manageWebhooks: $state")
+    }
+    override fun setViewChannel(state: PermissionState) {
+        println("viewChannel: $state")
+    }
+    override fun setConnect(state: PermissionState) {
+        println("connect: $state")
+    }
+    override fun setSpeak(state: PermissionState) {
+        println("speak: $state")
+    }
+    override fun setMuteMembers(state: PermissionState) {
+        println("muteMembers: $state")
+    }
+    override fun setDeafenMembers(state: PermissionState) {
+        println("deafenMembers: $state")
+    }
+    override fun setMoveMembers(state: PermissionState) {
+        println("moveMembers: $state")
+    }
+    override fun setUseVoiceActivity(state: PermissionState) {
+        println("useVoiceActivity: $state")
     }
 }
